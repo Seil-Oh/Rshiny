bf45aa4a1baa9141214a06b7047e41b4184cbbac
Seil Oh <so873@nyu.edu>
2018-09-27 12:34:49 +0900

    add data

---
+++ global.R
@@ -0,0 +1,21 @@
+
+setwd("D:/GoogleDrive/NPS/Rshiny/")
+
+TOT_IDX_DATA <- read_rds("TOT_IDX_DATA.rds")
+KB_IDX_RET_DATA <- read_rds("KB_IDX_RET_DATA.rds")
+
+con = gzcon(url('http://www.systematicportfolio.com/sit.gz', 'rb'))
+source(con)
+close(con)
+
+match.idx <- read.csv(file = "D:/GoogleDrive/NPS/Rshiny/MATCH_IDX.csv", 
+                      header = T) %>% 
+  as_tibble() %>% mutate_if(is.factor, as.character) %>% select(-X)
+
+etf.ticker.name <- match.idx %>% 
+  select(TICKER_NAME) %>% arrange(TICKER_NAME) %>% pull
+
+RAW_KB_ETF_LIST <- read.xlsx(xlsxFile = "KB_ETF_HOLD.xlsx", 
+                             sheet = 1, colNames = T) %>% as_tibble()
+
+
